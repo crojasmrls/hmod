57fb50bed05576c13a49b72e64117dd07fa16258
Carlos Rojas Morales <crojasmrls@gmail.com>
2021-10-21 20:58:27 -0500

    Fix pycharm warnings and include instr decode

--- fetch_lib.py
+++ fetch_lib.py
@@ -1,17 +1,22 @@
 import salabim as sim
-import Instr_lib as instr
+import instr_lib as instr
+
 
 class BasicInstrBlock:
     def __init__(self, name):
         self.name = name
         self.next_block = 'END'
         self.instr = []
-    def add_instr(self, instr):
-        self.instr.append(instr)
+
+    def add_instr(self, new_instr):
+        self.instr.append(new_instr)
+
     def set_next_block(self, next_block):
         self.next_block = next_block
+
     def get_instr(self, index):
         return self.instr[index]
+
 
 class InstrCache(sim.Component):
     """docstring for InstrCache"""
@@ -25,9 +30,10 @@ class InstrCache(sim.Component):
         self.branch_taken = False
         self.rob = rob
         self.take_branch = False
+
     def read_program(self):
         with sim.ItemFile('../programs/'+self.program) as f:
-            bb_nameprev = ''
+            bb_name_prev = ''
             bb_name = ''
             instr_count = 0
             while True:
@@ -37,61 +43,60 @@ class InstrCache(sim.Component):
                         instr_count = 0
                         bb_name = read_item[1:len(read_item)]
                         if len(self.bb_dict) > 0:
-                            self.bb_dict[bb_nameprev].set_next_block(bb_name)
+                            self.bb_dict[bb_name_prev].set_next_block(bb_name)
                         if len(self.bb_dict) == 0:
                             self.first_block = bb_name
                         self.bb_dict[bb_name] = BasicInstrBlock(bb_name)
-                        bb_nameprev = bb_name
+                        bb_name_prev = bb_name
                     else:
-                        instr = ''
+                        instr_buf = ''
                         while read_item != ':':
-                            if instr == '':
-                                instr = read_item
+                            if instr_buf == '':
+                                instr_buf = read_item
                             else:
-                                instr = instr + ' ' + read_item
+                                instr_buf = instr_buf + ' ' + read_item
                             read_item = f.read_item()
                         self.bb_dict[bb_name].add_instr(instr)
                         instr_count = instr_count + 1
                 except EOFError:
                     break
+
     def print_program(self):
         bb_name = self.first_block
-        while (bb_name != 'END'):
+        while bb_name != 'END':
             print(self.bb_dict[bb_name].name)
-            for instr in self.bb_dict[bb_name].instr:
-                print('    ' + instr)
+            for instr_aux in self.bb_dict[bb_name].instr:
+                print('    ' + instr_aux)
             bb_name = self.bb_dict[bb_name].next_block
+
     def send_first_bb(self):
         return self.first_block
-    def send_intrs(self, bb_name, offset):
-        instr.Instr(fetch_unit=self)
-        print(self.bb_dict[bb_name].instr[offset])
+
+    def send_instr(self, bb_name, offset):
+        instr.Instr(fetch_unit=self, instr=self.bb_dict[bb_name].instr[offset])
         self.rob.add_instr()
-        #return self.bb_dict[bb_name].instr[offset]
+        return self.bb_dict[bb_name].instr[offset]
 
     def change_pc(self, bb_name_branch):
         self.branch_taken = True
-        self.next_inst = self.send_intrs(bb_name_branch, 0)
+        self.next_inst = self.send_instr(bb_name_branch, 0)
 
-
-    
     def process(self):
-       # while self.bb_name != 'END':
         while self.bb_name != 'END' or self.rob.count_inst != 0:
-            if self.take_branch == False:
-                #self.offset = self.offset + 1
+            if not self.take_branch:
+                # self.offset = self.offset + 1
                 pass
             else:
                 pass
             if self.bb_name == 'END':
                 break
             else:
-                self.next_inst = self.send_intrs(self.bb_name, self.offset)
+                self.next_inst = self.send_instr(self.bb_name, self.offset)
             if self.offset == len(self.bb_dict[self.bb_name].instr) - 1:
 
                 self.bb_name = self.bb_dict[self.bb_name].next_block
                 self.offset = 0
-                while(len(self.bb_dict[self.bb_name].instr)==0):
+                while len(self.bb_dict[self.bb_name].instr) == 0:
                     if self.bb_name == 'END':
                         break
                     self.bb_name = self.bb_dict[self.bb_name].next_block
@@ -111,4 +116,4 @@ class InstrCache(sim.Component):
 #     def __init__(self, arg):
 #         super(request, self).__init__()
 #         self.arg = arg
-#         
+#
--- read-pseudocode.py
+++ read-pseudocode.py
@@ -1,15 +1,18 @@
 
 import salabim as sim
 import fetch_lib as fetch
-import Instr_lib as instr
-import ReorderBuffer_lib as ROB
+import reorderbuffer_lib as rob
 
 program = 'binary_search.HS'
 
-
 env = sim.Environment(trace=True)
-rob= ROB.ReorderBuffer()
-InstrCache_inst = fetch.InstrCache(program=program, env1=env, rob=rob)
+
+# Queues creation
+
+IntQueue = sim.Queue("IntQueue")
+HQueue = sim.Queue("HQueue")
+Rob = rob.ReorderBuffer()
+InstrCache_inst = fetch.InstrCache(program=program, env1=env, rob=Rob)
 InstrCache_inst.read_program()
 
 program_begin = InstrCache_inst.send_first_bb()
