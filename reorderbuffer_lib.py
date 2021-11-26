6300ac9efe71aaea3c33d56b862f26f90379cac1
Carlos Rojas Morales <crojasmrls@gmail.com>
2021-11-26 11:16:23 -0600

    Add reorder buffer rresources

--- reorderbuffer_lib.py
+++ reorderbuffer_lib.py
@@ -1,6 +1,10 @@
-class ReorderBuffer:
-    def __init__(self):
-        self.instrs = []
+import salabim as sim
+
+
+class ReorderBuffer(sim.Component):
+    def setup(self, rob_entries):
+        self.rob_entries = rob_entries
+        self.rob_resource = sim.Resource('rob_resource', capacity=self.rob_entries)
         self.count_inst = 0
 
     def instr_end(self):
