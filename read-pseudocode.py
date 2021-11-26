74d16d5f759e1bb712e769b5e5cb2a8ca45eaa3a
Carlos Rojas Morales <crojasmrls@gmail.com>
2021-11-26 11:15:28 -0600

    Add procesor node intance

--- read-pseudocode.py
+++ read-pseudocode.py
@@ -1,20 +1,18 @@
 
 import salabim as sim
-import fetch_lib as fetch
-import reorderbuffer_lib as rob
+import pe_lib as pe
 
-program = 'binary_search.HS'
+
+program = 'add.HS'
 
 env = sim.Environment(trace=True)
 
-# Queues creation
+# 
 
-int_queue = sim.Queue("int_queue")
-h_queue = sim.Queue("h_queue")
-Rob = rob.ReorderBuffer()
-InstrCache_inst = fetch.InstrCache(program=program, env1=env, rob=Rob, int_queue=int_queue, h_queue=h_queue)
-InstrCache_inst.read_program()
+processor_node1 = pe.PE(fetch_width=2, physical_registers=64, int_alus=3, rob_entries=128, program=program)
 
-program_begin = InstrCache_inst.send_first_bb()
+
+processor_node1.InstrCache_inst.read_program()
+
 
 env.run(till=500)
