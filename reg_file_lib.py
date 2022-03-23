431ec48d8a3783422627e04f9322f2f2b27f8005
Carlos Rojas Morales <crojasmrls@gmail.com>
2022-03-23 15:13:09 +0100

    Reg file implemented as regular python object

--- reg_file_lib.py
+++ reg_file_lib.py
@@ -1,8 +1,8 @@
 import salabim as sim
 
 
-class RegFile(sim.Component):
-    def setup(self, physical_registers):
+class RegFile:
+    def __init__(self, physical_registers):
         self.FRL_resource = sim.Resource('FRL_resource', capacity=physical_registers-32)
         self.rat = [PhysicalRegister(state=True, value=i) for i in range(32)]
 
@@ -12,16 +12,8 @@ class RegFile(sim.Component):
     def get_reg(self, arch_reg):
         return self.rat[arch_reg]
 
-    def process(self):
-        while True:
-            yield self.hold(1)
 
-
-class PhysicalRegister(sim.Component):
-    def setup(self, state=False, value=0):
+class PhysicalRegister:
+    def __init__(self, state=False, value=0):
         self.reg_state = sim.State("ready_bit", value=state)
         self.value = value
-
-    def process(self):
-        while True:
-            yield self.hold(1)
