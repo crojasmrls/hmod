c7dab33419fd9d17675fcb00ad064b94144765e8
Carlos Rojas Morales <carlos.rojas@bsc.es>
2022-11-17 19:34:19 +0100

    Increase resource number to avoid stalls in the Front end

--- src/pipeline_parameters_1.py
+++ src/pipeline_parameters_1.py
@@ -1,10 +1,11 @@
 class PipelineParams:
     fetch_width = 2
-    physical_registers = 64
+    physical_registers = 128
     int_alus = 2
     branch_units = 1
     rob_entries = 128
-    int_queue_slots = 16
-    lsu_slots = 16
+    int_queue_slots = 32
+    lsu_slots = 64
     brob_entries = 32
     branch_in_int_alu = True
+    exe_brob_release = True
