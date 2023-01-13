3468ae0aded6955c6d3a52dc6263a8f07bc02b01
Carlos Rojas Morales <carlos.rojas@bsc.es>
2023-01-13 00:39:41 -0600

    :art: Add format changes

--- src/pe_lib.py
+++ src/pe_lib.py
@@ -5,12 +5,13 @@ import resources_lib as res
 class PE:
     def __init__(self, params, program, thread_id, konata_signature):
         # Konata
-        self.konata_signature=konata_signature
+        self.konata_signature = konata_signature
         # Parameters
         self.params = params
         self.thread_id = thread_id
         # Resources
         self.ResInst = res.Resources(params=self.params)
         # Instr cache + fetch engine
-        self.InstrCacheInst = fetch.InstrCache(program=program, params=params, resources=self.ResInst, thread_id=self.thread_id,
-            konata_signature=self.konata_signature, priority=100)
+        self.InstrCacheInst = fetch.InstrCache(program=program, params=params, resources=self.ResInst,
+                                               thread_id=self.thread_id, konata_signature=self.konata_signature,
+                                               priority=100)
