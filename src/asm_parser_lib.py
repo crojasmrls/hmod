5f668d12682dc7c2436232535f116e5509d249b8
Carlos Rojas Morales <crojasmrls@gmail.com>
2023-08-17 21:50:20 +0200

    :sparkles: Add ASM parser lib

---
+++ src/asm_parser_lib.py
@@ -0,0 +1,34 @@
+class ASMParser:
+    def __init__(self, data_cache, instr_cache):
+        self.data_cache = data_cache
+        self.instr_cache = instr_cache
+
+    def add_instr(self, new_instr):
+        self.instr.append(new_instr)
+
+
+    def read_program(self, program):
+        bb_name_prev = ''
+        bb_name = ''
+        instr_count = 0
+        f = open(program, "r")
+        lines = f.readlines()
+        line_number = 0
+        for line in lines:
+            line_number += 1
+            line = line.replace("\n", "")
+            line = line.replace("\t", " ")
+            line = line.split('#')[0]
+            # If the line is a code segment tag
+            if line.find(':') != -1:
+                instr_count = 0
+                # Remove the code segment tag indicator
+                bb_name = line.split(':')[0].split()[0]
+                if len(bb_name) != 0:
+                    self.instr_cache.add_bb(bb_name,bb_name_prev)
+                    self.bb_dict[bb_name] = BasicInstrBlock(bb_name)
+                    bb_name_prev = bb_name
+            else:
+                if len(line.replace(" ", "")) != 0:
+                    self.instr_cache.add_instr(bb_name, (line, line_number))
+                    instr_count = instr_count + 1
