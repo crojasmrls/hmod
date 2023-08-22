1404be5d96852491bff1b120bf111615140d4681
Carlos Rojas Morales <crojasmrls@gmail.com>
2023-08-22 18:09:50 +0200

    Add some static methods to improve code readness

--- src/asm_parser_lib.py
+++ src/asm_parser_lib.py
@@ -2,19 +2,17 @@ class ASMParser:
     def __init__(self, data_cache, instr_cache):
         self.data_cache = data_cache
         self.instr_cache = instr_cache
+        self.constant_dict = {}
 
     def read_program(self, program):
         bb_name_prev = ''
         bb_name = ''
         instr_count = 0
-        f = open(program, "r")
-        lines = f.readlines()
+        lines = self.get_file_lines(program)
         line_number = 0
         for line in lines:
             line_number += 1
-            line = line.replace("\n", "")
-            line = line.replace("\t", " ")
-            line = line.split('#')[0]
+            line = self.clean_line(line)
             # If the line is a code segment tag
             if line.find(':') != -1:
                 instr_count = 0
@@ -27,3 +25,22 @@ class ASMParser:
                 if len(line.replace(" ", "")) != 0:
                     self.instr_cache.add_instr(bb_name, (line, line_number))
                     instr_count = instr_count + 1
+
+    def fill_constant_dict(self, program):
+        lines = self.get_file_lines(program)
+        for line in lines:
+            pass
+
+    @staticmethod
+    def get_file_lines(program):
+        file = open(program, "r")
+        lines = file.readlines()
+        file.close()
+        return lines
+
+    @staticmethod
+    def clean_line(line):
+        line = line.replace("\n", "")
+        line = line.replace("\t", " ")
+        line = line.split('#')[0]
+        return line
