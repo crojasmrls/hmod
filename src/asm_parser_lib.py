56079dccd1040d6b7a77c749a2474f6945ffcf41
Carlos Rojas Morales <crojasmrls@gmail.com>
2023-09-01 18:31:23 +0200

    :sparkles: Add ASM parsing functions for imediate substitutions

--- src/asm_parser_lib.py
+++ src/asm_parser_lib.py
@@ -1,4 +1,5 @@
 from enum import Enum, IntEnum, auto
+import re
 
 
 class Sections(Enum):
@@ -29,21 +30,43 @@ class ASMParser:
         instr_count = 0
         lines = self.get_file_lines(program)
         line_number = 0
+        section = Sections.HEAD
         for line in lines:
             line_number += 1
             line = self.clean_line(line)
             # If the line is a code segment tag
-            if ':' in line:
-                instr_count = 0
-                # Remove the code segment tag indicator
-                bb_name = self.get_tag_name(line)
-                if len(bb_name) != 0:
+            if section is Sections.MAIN:
+                if '.-main' in line:
+                    break
+                if ':' in line:
+                    instr_count = 0
+                    # Remove the code segment tag indicator
+                    bb_name = self.get_tag_name(line)
+                    if len(bb_name) != 0:
+                        self.instr_cache.add_bb(bb_name, bb_name_prev)
+                        bb_name_prev = bb_name
+                else:
+                    if len(line.replace(" ", "")) != 0:
+                        if '%hi' in line:
+                            address = self.get_address(line)
+                            offset = self.get_offset(line)
+                            immediate = self.get_hi(address, offset)
+                            line = self.replace_high(line, immediate)
+                        if '%lo' in line:
+                            address = self.get_address(line)
+                            offset = self.get_offset(line)
+                            immediate = self.get_lo(address, offset)
+                            line = self.replace_low(line, immediate)
+                        self.instr_cache.add_instr(bb_name, (line, line_number))
+                        instr_count = instr_count + 1
+            else:
+                if 'main:' in line:
+                    section = Sections.MAIN
+                    instr_count = 0
+                    # Remove the code segment tag indicator
+                    bb_name = self.get_tag_name(line)
                     self.instr_cache.add_bb(bb_name, bb_name_prev)
                     bb_name_prev = bb_name
-            else:
-                if len(line.replace(" ", "")) != 0:
-                    self.instr_cache.add_instr(bb_name, (line, line_number))
-                    instr_count = instr_count + 1
 
     def fill_data(self, program, mem_map):
         section = Sections.HEAD
@@ -82,19 +105,38 @@ class ASMParser:
                     section = Sections.TEXT
                     address = mem_map.RODATA
 
-    @staticmethod
-    def get_file_lines(program):
-        file = open(program, "r")
-        lines = file.readlines()
-        file.close()
-        return lines
+    def get_address(self, line):
+        tag = re.findall(r'\(([^$]*)\)', line)[0].split('+')[0].split()[0]
+        return self.constant_dict[tag]
 
     @staticmethod
-    def clean_line(line):
-        line = line.replace("\n", "")
-        line = line.replace("\t", " ")
-        line = line.split('#')[0]
-        return line
+    def get_offset(line):
+        tag = re.findall(r'\(([^$]*)\)', line)[0]
+        try:
+            offset = tag.split('+')[1]
+        except IndexError:
+            return 0
+        else:
+            try:
+                return int(offset)
+            except ValueError:
+                return 0
+
+    @staticmethod
+    def get_hi(address, offset):
+        return str(((address + offset) & 0xfffff000) >> 12)
+
+    @staticmethod
+    def get_lo(address, offset):
+        return str((address + offset) & 0x00000fff)
+
+    @staticmethod
+    def replace_high(line, immediate):
+        return re.sub(r'%hi\(?(.*?)\)', immediate, line)
+
+    @staticmethod
+    def replace_low(line, immediate):
+        return re.sub(r'%lo\(?(.*?)\)', immediate, line)
 
     @staticmethod
     def get_tag_name(line):
@@ -111,3 +153,17 @@ class ASMParser:
     @staticmethod
     def get_int_data(line):
         return int(line.split()[1].split()[0])
+
+    @staticmethod
+    def get_file_lines(program):
+        file = open(program, "r")
+        lines = file.readlines()
+        file.close()
+        return lines
+
+    @staticmethod
+    def clean_line(line):
+        line = line.replace("\n", "")
+        line = line.replace("\t", " ")
+        line = line.split('#')[0]
+        return line
